16041f194b70f88061d94fe2579e60fe405dc383
Jack <jack20220723@gmail.com>
2024-01-12 19:32:23 +0800

    Change group ID to org.qubitpi.athena

---
+++ athena-metastore/athena-metastore-graphql/src/main/java/org/qubitpi/athena/metastore/graphql/query/GraphQLQueryProvider.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright Jiaqi Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qubitpi.athena.metastore.graphql.query;
+
+import org.qubitpi.athena.metadata.MetaData;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+/**
+ * {@link GraphQLQueryProvider} is an abstraction layer that constructs GraphQL queries on file metadata.
+ * <p>
+ * Implementation must be package-scoped and let {@link GraphQLQueryProviderFactory} as the broker for their instance
+ * provision
+ */
+public interface GraphQLQueryProvider {
+
+    /**
+     * Constructs and returns a GraphQL file metadata Query document given the specified file ID associated with the
+     * requested metadata and the metadata fields.
+     * <p>
+     * For example, if file ID is "df32wsv3rr3ed", and the requested metadata fields are ["fileType", "fileName"], this
+     * method will return a string of
+     * <pre>
+     * {@code
+     * query {
+     *     metaData(fileId: "df32wsv3rr3ed") {
+     *         fileType
+     *         fileName
+     *     }
+     * }
+     * }
+     * </pre>
+     *
+     * @param fileId  The file ID associated with the requested metadata info
+     * @param metadataFields  The requested metadata fields
+     *
+     * @return a GraphQL query
+     *
+     * @throws NullPointerException if {@code fileId} or {@code metadataFields} is {@code null}
+     * @throws IllegalArgumentException if {@code metadataFields} list is empty
+     */
+    @NotNull
+    String query(@NotNull String fileId, @NotNull List<String> metadataFields);
+
+    /**
+     * Constructs and returns a GraphQL file metadata Mutation document given a {@link MetaData} object to be saved and
+     * a specified file ID associated with the metadata.
+     * <p>
+     * For example, if file ID is "df32wsv3rr3ed", and the metadata to be saved is
+     * <code>{"fileType": "PDF", "fileName": "Pride and Prejudice"}</code>, then this method will return a string of
+     * <pre>
+     * {@code
+     * mutation createMetaData {
+     *     createMetaData(fileId: "df32wsv3rr3ed", fileName: "Pride and Prejudice", fileType: "PDF") {
+     *         fileName,
+     *         fileType
+     *     }
+     * }
+     * }
+     * </pre>
+     *
+     * @param fileId  The file ID associated with the requested metadata info
+     * @param metaData  An object that contains all information about the new metadata to be saved
+     *
+     * @return a GraphQL query that creates a new {@link MetaData} object in database
+     *
+     * @throws NullPointerException if {@code fileId} or {@code metaData} is {@code null}
+     */
+    @NotNull
+    String mutation(@NotNull String fileId, @NotNull MetaData metaData);
+}
---
+++ athena-system-config/src/main/java/org/qubitpi/athena/config/SystemConfig.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright Jiaqi Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qubitpi.athena.config;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+
+/**
+ * {@link SystemConfig} is an interface for retrieving configuration values, allowing for implicit type conversion and
+ * use of a runtime properties interface to override configured settings.
+ */
+public interface SystemConfig {
+
+    /**
+     * Sets property value for a key.
+     *
+     * @param key  The key of the property to change
+     * @param value  The value to set to
+     *
+     * @throws NullPointerException if either {@code key} or {@code value} is {@code null}
+     */
+    default void setProperty(final @NotNull String key, final @NotNull String value) {
+        getRuntimeProperties().setProperty(Objects.requireNonNull(key), Objects.requireNonNull(value));
+    }
+
+    /**
+     * Updates a property from the user-defined runtime configuration.
+     *
+     * @param key  The key of the property to update
+     * @param value  The value to set to
+     *
+     * @throws NullPointerException if either {@code key} or {@code value} is {@code null}
+     */
+    default void resetProperty(final @NotNull String key, final @NotNull String value) {
+        setProperty(key, value);
+    }
+
+    /**
+     * Removes property from the use-defined runtime configuration.
+     *
+     * @param key  The key of the property to remove
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    default void clearProperty(final @NotNull String key) {
+        getRuntimeProperties().remove(Objects.requireNonNull(key));
+    }
+
+    /**
+     * Gets a package scoped variable name.
+     *
+     * @param suffix  The variable name of the configuration variable without the package prefix
+     *
+     * @return variable name
+     *
+     * @throws NullPointerException if {@code suffix} is {@code null}
+     */
+    @NotNull
+    String getPackageVariableName(@NotNull String suffix);
+
+    /**
+     * Returns property value as String value wrapped inside an {@link Optional} for a key or
+     * {@link Optional#empty() empty} if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<String> getStringProperty(@NotNull String key);
+
+    /**
+     * Returns property value as int value wrapped inside an {@link Optional} for a key or
+     * {@link Optional#empty() empty} if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<Integer> getIntProperty(@NotNull String key);
+
+    /**
+     * Returns property value as boolean value wrapped inside an {@link Optional} for a key or
+     * {@link Optional#empty() empty} if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<Boolean> getBooleanProperty(@NotNull String key);
+
+    /**
+     * Returns property value as long value wrapped inside an {@link Optional} for a key or
+     * {@link Optional#empty() empty} if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<Long> getLongProperty(@NotNull String key);
+
+    /**
+     * Returns property value as a double wrapped inside an {@link Optional} for a key or {@link Optional#empty() empty}
+     * if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<Double> getDoubleProperty(@NotNull String key);
+
+    /**
+     * Returns property value as float value wrapped inside an {@link Optional} for a key or
+     * {@link Optional#empty() empty} if no such property exists.
+     *
+     * @param key  The key for which value needs to be fetched
+     *
+     * @return an {@link Optional} value for the requested key or {@link Optional#empty()}
+     *
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    @NotNull
+    Optional<Float> getFloatProperty(@NotNull String key);
+
+    /**
+     * Returns the properties used to hold the highest-priority config values.
+     * <p>
+     * <b>This method is intended primarily for interface support and not for client interactions</b>
+     * <p>
+     * Notes: this is a design flaw based on how it is used in method such as {@link #setProperty(String, String)}. If
+     * implementations produces a shallow copy of the runtime properties, then {@link #setProperty(String, String)} will
+     * fail to accomplish what it supposed to do. The fact that this method is "not being called by client" pushes us to
+     * look at problem at a higher level: this method assumes the returned properties are directly mutable and will
+     * mutate the config instance directly. This violates encapsulation. we should remove this method and ask client to
+     * implement {@link #setProperty(String, String)}
+     *
+     * @return a properties object which act as a runtime mask against other configuration properties
+     */
+    @NotNull
+    Properties getRuntimeProperties();
+}
