3d9f7fafa47a63d7464f4494754965ceafd83d86
Jack <jack20220723@gmail.com>
2024-05-13 21:50:11 +0800

    New logo

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2024 Jiaqi Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
@@ -7,8 +22,8 @@ const config: Config = {
   tagline: 'A GraphQL Object Storage Webservice Backed by OpenStack Swift and Hadoop HDFS',
   favicon: 'img/favicon.ico',
 
-  url: 'https://qubitpi.github.io/',
-  baseUrl: '/athena',
+  url: 'https://athena.qubitpi.org',
+  baseUrl: '/',
 
   organizationName: 'QubitPi',
   projectName: 'athena',
@@ -61,7 +76,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: "https://qubitpi.github.io/athena/apidocs",
+          href: "https://athena.qubitpi.org/apidocs",
           label: "API",
           position: "left",
         },
